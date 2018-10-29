ca6a5dc05af22864a88200dfad02b0769af450ea
kmamiit <kmamiit123@gmail.com>
2018-10-28 17:00:25 -0700

    error: extra argument in call

---
+++ Pods/Target Support Files/Pods-instaCloneUITests/Pods-instaCloneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_instaCloneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_instaCloneUITests
+@end
---
+++ instaClone/LoginViewController.swift
@@ -0,0 +1,97 @@
+//
+//  LoginViewController.swift
+//  instaClone
+//
+//  Created by Kyle Mamiit (New) on 10/27/18.
+//  Copyright © 2018 Kyle Mamiit. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logIn(withUsername: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+            }
+        }
+    }
+   
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        //newUser.email =
+        newUser.password = passwordField.text
+        
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("Yay, created a user! User Registered Successfully")
+            } else if let error = error {
+                print(error.localizedDescription);
+                if error._code == 202 {
+                    print("Username is taken");
+                }
+            }
+            }
+        /*
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered Successfully")
+            }
+        }
+        */
+    }
+    /*
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: NSError?) -> Void in
+            if success {
+                print("yay, created a user!")
+            } else {
+                print(error?.localizedDescription);
+            }
+        }
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
